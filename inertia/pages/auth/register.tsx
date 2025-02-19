e104c984b4e1b7d7b96103ed744782791de3c36b
Damien Dumontet <damien.dumontet@protonmail.com>
2025-02-19 18:33:14 -0300

    add shadcn

--- inertia/pages/auth/register.tsx
+++ inertia/pages/auth/register.tsx
@@ -1,10 +1,12 @@
 import { Head } from '@inertiajs/react'
+import { Button } from '~/components/ui/button'
 
 export default function Register() {
   return (
     <>
       <Head title="Register" />
       <div className="text-4xl">Register</div>
+      <Button variant="default">Button</Button>
     </>
   )
 }
