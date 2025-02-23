2db0ff43c26f6a33c2e7338883034e0f4a986ab2
Damien Dumontet <damien.dumontet@protonmail.com>
2025-02-23 13:30:48 -0300

    add user

--- app/routes/_protected/index.tsx
+++ app/routes/_protected/index.tsx
@@ -1,4 +1,11 @@
-import { Button } from '@heroui/react';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  User,
+} from '@heroui/react';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { authClient } from '~/lib/auth-client';
 
@@ -20,9 +27,51 @@ function RouteComponent() {
   };
 
   return (
-    <div>
-      <h1>Home</h1>
-      <Button onPress={handleSignout}>Sign out</Button>
+    <div className="flex flex-col h-screen">
+      <div className="flex items-center h-14 border-y border-divider">
+        <div></div>
+        <div className="flex items-center ml-auto border-l border-divider h-full px-8 hover:bg-content2 cursor-pointer">
+          <Dropdown
+            classNames={{
+              content:
+                'rounded-none mt-1 shadow-none border-x border-b border-divider',
+            }}
+          >
+            <DropdownTrigger>
+              <User
+                avatarProps={{
+                  src: 'https://i.pravatar.cc/150?u=a04258114e29026302d',
+                  isBordered: true,
+                  size: 'sm',
+                }}
+                description="damien.dumontet.perso@gmail.com"
+                name="Damien Dumontet"
+                as="button"
+              />
+            </DropdownTrigger>
+            <DropdownMenu
+              variant="solid"
+              color="primary"
+              aria-label="Static Actions"
+            >
+              <DropdownItem
+                key="settings"
+                classNames={{ base: 'rounded-none' }}
+              >
+                Settings
+              </DropdownItem>
+              <DropdownItem
+                key="signout"
+                classNames={{ base: 'rounded-none' }}
+                onPress={handleSignout}
+              >
+                Sign out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </div>
+      <div className="h-full"></div>
     </div>
   );
 }
