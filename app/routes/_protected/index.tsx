953d1267dbad520886b096323e7ef57af1c2bfed
Damien Dumontet <damien.dumontet@protonmail.com>
2025-02-24 17:09:16 -0300

    add menu

--- app/routes/_protected/index.tsx
+++ app/routes/_protected/index.tsx
@@ -7,6 +7,14 @@ import {
   User,
 } from '@heroui/react';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import {
+  Building2,
+  GripIcon,
+  LayoutDashboardIcon,
+  PlusIcon,
+  SettingsIcon,
+  SquareKanbanIcon,
+} from 'lucide-react';
 import { authClient } from '~/lib/auth-client';
 
 export const Route = createFileRoute('/' as never)({
@@ -33,12 +41,39 @@ function RouteComponent() {
           <div className="flex items-center h-full px-8 border-r border-divider">
             <p className="font-display text-lg">spydem</p>
           </div>
+          <div className="flex items-center justify-center gap-2 px-8">
+            <GripIcon className="w-8" />
+            <p className="">Dashboard</p>
+          </div>
         </div>
-        <div className="h-full"></div>
+        <div className="flex flex-col gap-8 h-full p-8">
+          <div className="flex items-center justify-start gap-2">
+            <GripIcon className="w-6" />
+            <p>Dashboard</p>
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <SettingsIcon className="w-6" />
+            <p>Spy settings</p>
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <p className="text-default-400 text-md font-light">Companies</p>
+          </div>
+
+          <Button
+            radius="none"
+            variant="solid"
+            color="primary"
+            size="lg"
+            className="mt-auto"
+            startContent={<PlusIcon className="w-6" />}
+          >
+            Add company
+          </Button>
+        </div>
       </div>
       <div className="flex flex-col h-full w-full">
         <div className="flex items-center h-14 border-y border-divider">
-          <div></div>
+          <div className="px-8"></div>
           <div className="flex items-center ml-auto border-l border-divider h-full px-8 hover:bg-content2 cursor-pointer">
             <Dropdown
               classNames={{
