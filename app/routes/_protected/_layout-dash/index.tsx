479b06b7599543aff0019260470d54d227019a2e
Damien Dumontet <damien.dumontet@protonmail.com>
2025-02-25 20:18:00 -0300

    add form company spy

--- app/routes/_protected/_layout-dash/index.tsx
+++ app/routes/_protected/_layout-dash/index.tsx
@@ -1,9 +1,45 @@
+import { Button, Input } from '@heroui/react';
 import { createFileRoute } from '@tanstack/react-router';
+import { ArrowRightIcon } from 'lucide-react';
+import { useState } from 'react';
 
 export const Route = createFileRoute('/' as never)({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Dashboard</div>;
+  const [companyName, setCompanyName] = useState('');
+
+  return (
+    <div className="flex flex-col  h-full items-center justify-center">
+      <div className="flex flex-col gap-16 max-w-[600px]">
+        <h1 className="font-display text-5xl text-center">
+          enter the name of the company you want to spy
+        </h1>
+
+        <form className="flex items-center gap-4">
+          <Input
+            radius="none"
+            label="company name"
+            labelPlacement="inside"
+            name="company"
+            type="text"
+            value={companyName}
+            variant="faded"
+            onChange={(e) => setCompanyName(e.target.value)}
+          />
+          <Button
+            size="lg"
+            type="submit"
+            color="primary"
+            radius="none"
+            endContent={<ArrowRightIcon className="w-12" />}
+            className="h-full"
+          >
+            Spy
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
